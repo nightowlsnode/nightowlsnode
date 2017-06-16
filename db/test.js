abc4f10e0d607ae16cc5ba87e04e1e14b3570711
Ben Cooley <BMCooley@gmail.com>
2017-06-16 10:55:54 -0400

    Add dummy data

---
+++ db/test.js
@@ -0,0 +1,42 @@
+exports.items = [
+  {
+    id: 0,
+    title: 'Crock Pot',
+    image: 'http://www.crock-pot.com/dw/image/v2/AAMB_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8c9b92f6/images/highres/SCCPVI600-S-1.jpg?sw=500&sh=500&sm=fit'
+    owner: 1,
+    borrower: null
+  },
+  {
+    id: 1,
+    title: 'Rice Cooker',
+    image: 'https://target.scene7.com/is/image/Target/50972594_Alt02?wid=520&hei=520&fmt=pjpeg'
+    owner: 0,
+    borrower: null
+  },
+  {
+    id: 2,
+    title: 'Schlong',
+    image: 'http://www.pngmart.com/files/1/Eggplant-PNG-Photos.png'
+    owner: 1,
+    borrower: 0
+  }
+]
+
+exports.users = [
+  {
+    id: 0,
+    name: 'Bill Cosby',
+    image: 'http://i2.cdn.cnn.com/cnnnext/dam/assets/151230191337-bill-cosby-arraignment-large-169.jpg'
+    bio: 'I\'m ya boy boo-boo',
+    rating: 3.8,
+    ratingCount: 200
+  },
+  {
+    id: 1,
+    name: 'Barack Obama',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/220px-President_Barack_Obama.jpg'
+    bio: 'Ham bone, Ham bone Where you live?',
+    rating: 4.7,
+    ratingCount: 15
+  }
+]
